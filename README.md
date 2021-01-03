d887bc39811fb74d5ab7246063701d944d9f95e5
Takuya Kikuchi <kikuchi.takuya.dbce8@gmail.com>
2021-01-03 16:07:17 +0900

    [FEATURE](App) Install Semantic-UI-React

--- README.md
+++ README.md
@@ -1,8 +1,15 @@
-# Software used for this application
+# Software used for this app
 - React
 - TypeScript
+- SCSS
 - ESLint
 - Prettier
 - stylelint
 - Husky
 - lint-staged
+- [Semantic UI React](https://react.semantic-ui.com/)
+
+# Methodology applied in this app
+
+- [Atomic Design](https://bradfrost.com/blog/post/atomic-web-design/)
+- [BEM](http://getbem.com/)
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Button } from 'semantic-ui-react';
 import './App.css';
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button>Click Here</Button>
     </div>
   );
 };
